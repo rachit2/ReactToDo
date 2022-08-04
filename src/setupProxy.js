8438a32f69f6d30d9ebc2cadf6d2610025e8b97d
Rachit Verma <rachitverma.001@gmail.com>
2022-08-04 14:12:32 +0530

    proxy changes

---
+++ src/setupProxy.js
@@ -0,0 +1,17 @@
+// const proxy = require("http-proxy-middleware");
+
+// module.exports = app => {
+//   app.use(proxy("/api/*", { target: "https://to-do-lists001.herokuapp.com/" }));
+// };
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://node-backend-to-do.herokuapp.com/',
+      changeOrigin: true,
+    })
+  );
+};
